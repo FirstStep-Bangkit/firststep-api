a87898c6690ffa73425ac9182ae94e6406fcd8e3
frnspry <frnspry@gmail.com>
2023-06-01 20:12:56 +0700

    change port

--- main.py
+++ main.py
@@ -213,4 +213,4 @@ api.add_resource(Survey, "/api/survey", methods=["GET"])
 api.add_resource(DeleteUser, "/api/deleteuser/<string:username>", methods=["DELETE"])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
