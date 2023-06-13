f9a10640503087b7b3b2f18850fbe27b4ef87747
frnspry <frnspry@gmail.com>
2023-06-14 02:39:42 +0700

    major change

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ import datetime
 from functools import wraps
 import numpy as np
 import tensorflow as tf
+import requests
+from werkzeug.utils import secure_filename
+from google.cloud import storage
+from google.oauth2 import service_account
+from google.auth import exceptions
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 # Mengambil credential
-
 MYSQL_HOST = os.environ.get('MYSQL_HOST')
 MYSQL_USER = os.environ.get('MYSQL_USER')
 MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
 MYSQL_DB = os.environ.get('MYSQL_DB')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+GCS_BUCKET_NAME = os.environ.get('GCS_BUCKET_NAME')
 
 # Konfigurasi database
 app.config['MYSQL_HOST'] = MYSQL_HOST
@@ -27,6 +32,7 @@ app.config['MYSQL_USER'] = MYSQL_USER
 app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
 app.config['MYSQL_DB'] = MYSQL_DB
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['GCS_BUCKET_NAME'] = GCS_BUCKET_NAME
 
 # Inisialisasi objek MySQL
 mysql = mysql.connector.connect(
@@ -49,9 +55,12 @@ CREATE TABLE IF NOT EXISTS auth_model (
     password VARCHAR(100) NOT NULL,
     status VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    mbti varchar(50)
+    mbti varchar(50),
+    photo_profile VARCHAR(255),
+    update_counter INT
 )
 """
+
 # Commit perubahan ke database
 mysql.commit()
 
@@ -60,7 +69,7 @@ cursor.execute(create_table_query)
 
 # Model database untuk authentication login register
 class AuthModel:
-    def __init__(self, username, frontName, lastName, email, password, status, created_at, mbti):
+    def __init__(self, username, frontName, lastName, email, password, status, created_at, mbti, photo_profile, update_counter):
         self.username = username
         self.frontName = frontName
         self.lastName = lastName
@@ -69,6 +78,8 @@ class AuthModel:
         self.status = status
         self.created_at = created_at
         self.mbti = mbti
+        self.photo_profile = photo_profile
+        self.update_counter = update_counter
 
 # Load the TensorFlow Lite model
 interpreter = tf.lite.Interpreter(model_path='model(1).tflite')
@@ -79,6 +90,7 @@ output_details = interpreter.get_output_details()
 # Define class labels
 class_labels = ['ESTJ', 'ENTJ', 'ESFJ', 'ENFJ', 'ISTJ', 'ISFJ', 'INTJ', 'INFJ', 'ESTP', 'ESFP', 'ENTP', 'ENFP', 'ISTP', 'ISFP', 'INTP', 'INFP']
 
+storage_client = storage.Client()
 
 # Decorator untuk kunci endpoint / authentication
 def token_required(f):
@@ -130,7 +142,9 @@ def get_current_user():
                 user['password'],
                 user['status'],
                 user['created_at'],
-                user['mbti']
+                user['mbti'],
+                user['photo_profile'],
+                user['update_counter']
             )
             return current_user
         else:
@@ -152,7 +166,8 @@ class RegisterUser(Resource):
             if user:
                 return make_response(jsonify({
                     "error": True,
-                    "msg": "Email sudah digunakan"}), 400)
+                    "msg": "Email sudah digunakan"
+                }), 400)
 
             cursor.execute("SELECT COUNT(*) FROM auth_model")
             result = cursor.fetchone()
@@ -161,7 +176,6 @@ class RegisterUser(Resource):
                 user_count = result['COUNT(*)']
             else:
                 user_count = 0
-
 
             default_status = "User"
             username = f"{default_status}{user_count+1}{datetime.datetime.now().strftime('%Y%m%d')}"
@@ -173,11 +187,13 @@ class RegisterUser(Resource):
 
             return make_response(jsonify({
                 "error": False,
-                "msg": "Registrasi Berhasil"}), 200)
+                "msg": "Registrasi Berhasil"
+            }), 200)
 
         return make_response(jsonify({
             "error": True,
-            "msg": "Email atau Password tidak boleh kosong"}), 400)
+            "msg": "Email atau Password tidak boleh kosong"
+        }), 400)
 
 class LoginUser(Resource):
     def post(self):
@@ -217,21 +233,25 @@ class LoginUser(Resource):
             "msg": "Login gagal, silahkan coba lagi !!!"
         })
 
-
 class Dashboard(Resource):
     @token_required
     def get(self):
         current_user = get_current_user()
-        
+
         if current_user.frontName and current_user.lastName:
             name = f"{current_user.frontName} {current_user.lastName}"
         elif current_user.frontName:
             name = current_user.frontName
 
         try:
+            if current_user.photo_profile:
+                profile_picture = current_user.photo_profile
+            else:
+                profile_picture = None
+
             dashboard_result = {
                 "name": name,
-                "profilePicture": None
+                "profilePicture": profile_picture
             }
 
             return jsonify({
@@ -245,19 +265,23 @@ class Dashboard(Resource):
                 "msg": "dashboard gagal"
             })
 
-
 class Profile(Resource):
     @token_required
     def get(self):
         current_user = get_current_user()
-        
+
         if current_user.frontName and current_user.lastName:
             name = f"{current_user.frontName} {current_user.lastName}"
         elif current_user.frontName:
             name = current_user.frontName
 
         try:
-            if current_user.mbti:  # Cek apakah pengguna memiliki nilai mbti
+            if current_user.photo_profile:
+                profile_picture = current_user.photo_profile
+            else:
+                profile_picture = None
+
+            if current_user.mbti:
                 mbti = current_user.mbti
             else:
                 mbti = None
@@ -265,9 +289,9 @@ class Profile(Resource):
             profile_result = {
                 "username": current_user.username,
                 "name": name,
-                "profilePicture": None,
+                "profilePicture": profile_picture,
                 "status": current_user.status,
-                "mbti": mbti  # Menggunakan variabel mbti yang ditentukan di atas
+                "mbti": mbti
             }
 
             return jsonify({
@@ -281,7 +305,6 @@ class Profile(Resource):
                 "msg": "Profile gagal"
             })
 
-
 class ChangePassword(Resource):
     @token_required
     def post(self):
@@ -292,7 +315,8 @@ class ChangePassword(Resource):
         if not dataCurrentPassword or not dataNewPassword:
             return make_response(jsonify({
                 "error": True,
-                "msg": "Password tidak boleh kosong"}), 400)
+                "msg": "Password tidak boleh kosong"
+            }), 400)
 
         # Periksa apakah password saat ini sesuai dengan yang tersimpan dalam basis data
         cursor.execute("SELECT * FROM auth_model WHERE email = %s", (current_user.email,))
@@ -305,11 +329,13 @@ class ChangePassword(Resource):
 
             return make_response(jsonify({
                 "error": False,
-                "msg": "Password berhasil diubah"}), 200)
+                "msg": "Password berhasil diubah"
+            }), 200)
         else:
             return make_response(jsonify({
                 "error": True,
-                "msg": "Password saat ini tidak valid"}), 401)
+                "msg": "Password saat ini tidak valid"
+            }), 401)
 
 class DeleteUser(Resource):
     @token_required
@@ -327,46 +353,47 @@ class DeleteUser(Resource):
 
         return jsonify({
             "error": True,
-            "message": "Anda tidak memiliki izin untuk menghapus pengguna ini"})
+            "message": "Anda tidak memiliki izin untuk menghapus pengguna ini"
+            })
 
 class Predict(Resource):
     @token_required
     def post(self):
         current_user = get_current_user()
         try:
-            # Get the input data
+            # Get input data
             data = request.json
             input_data = np.array(data['input']).astype(np.float32)  # Convert to FLOAT32
 
-            # Check if input length matches the expected length
+            # Memastikan jawaban berisi 60
             if len(input_data) != 60:
                 return jsonify({'error': 'Jawaban harus berisi 60'}), 400
 
             # Reshape input data
             input_data = np.reshape(input_data, (1, 60))
 
-            # Set the input tensor
+            # Set input tensor
             interpreter.set_tensor(input_details[0]['index'], input_data)
 
-            # Run inference
+            # Menjalankan inference
             interpreter.invoke()
 
-            # Get the output tensor
+            # Get output tensor
             output_data = interpreter.get_tensor(output_details[0]['index'])
             predicted_class = int(np.argmax(output_data))  # Convert to int
 
-            # Get the predicted label
+            # Get predicted label
             predicted_label = class_labels[predicted_class]
 
-            # Update the current user's MBTI
+            # Update current user MBTI
             current_user = get_current_user()
             current_user.mbti = predicted_label
 
-            # Save the predicted label to auth_model table
+            # Menyimpan predicted label ke table auth_model
             cursor.execute("UPDATE auth_model SET mbti = %s WHERE email = %s", (predicted_label, current_user.email))
             mysql.commit()
 
-            # Prepare the response
+            # Menyimpan response
             response = {
                 'predicted_class': predicted_class,
                 'predicted_label': predicted_label
@@ -410,6 +437,106 @@ class Personality(Resource):
                 return jsonify({'error': 'Data tidak ditemukan'}), 404
         else:
             return jsonify({'error': 'Pengguna tidak memiliki nilai mbti'}), 400
+        
+# Fungsi untuk mengunggah foto profil ke Google Cloud Storage
+def upload_photo_profile(file, filename):
+    bucket = storage_client.bucket(app.config['GCS_BUCKET_NAME'])
+    blob = bucket.blob(filename)
+    blob.upload_from_file(file)
+    blob.make_public()
+    return blob.public_url
+
+class UploadPhoto(Resource):
+    @token_required
+    def post(self):
+        current_user = get_current_user()
+        # Cek apakah file foto profil tersedia dalam request
+        if 'photo_profile' not in request.files:
+            return jsonify({'error': True, 'msg': 'Tidak memiliki foto profile'})
+
+        photo_profile = request.files['photo_profile']
+
+        # Cek apakah file yang diunggah adalah gambar
+        if photo_profile.mimetype not in ['image/jpeg', 'image/png']:
+            return jsonify({'error': True, 'message': 'Invalid photo profile format (jpg/png)'})
+
+        # Cek apakah pengguna sudah memiliki foto profil sebelumnya
+        cursor.execute("SELECT photo_profile, update_counter FROM auth_model WHERE username = %s", (current_user.username,))
+        user = cursor.fetchone()
+        if user and user['photo_profile']:
+            # Hapus foto profil sebelumnya di GCS
+            try:
+                bucket = storage_client.bucket(app.config['GCS_BUCKET_NAME'])
+                previous_update_counter = user['update_counter']
+                previous_extension = user['photo_profile'].split('.')[-1]
+                previous_filename = secure_filename(f"{current_user.username}_{previous_update_counter}.{previous_extension}")
+                blob = bucket.blob(previous_filename)
+                blob.delete()
+            except:
+                pass
+
+        # Generate nama file yang aman untuk foto profil dengan menggunakan username pengguna dan counter update
+        cursor.execute("SELECT update_counter FROM auth_model WHERE username = %s", (current_user.username,))
+        result = cursor.fetchone()
+        update_counter = result['update_counter'] + 1 if result and result['update_counter'] else 1
+        filename = secure_filename(f"{current_user.username}_{update_counter}.jpg")  # Ubah ekstensi file sesuai kebutuhan
+
+        # Upload foto profil ke GCS
+        photo_url = upload_photo_profile(photo_profile, filename)
+
+        # Simpan URL foto profil dan counter update ke database
+        cursor.execute("UPDATE auth_model SET photo_profile = %s, update_counter = %s WHERE username = %s", (photo_url, update_counter, current_user.username))
+        mysql.commit()
+
+        return jsonify({
+            'error': False,
+            'message': 'Photo profile berhasil di-upload',
+            'photo_url': photo_url
+        })
+    
+class DeletePhoto(Resource):
+    @token_required
+    def delete(self):
+        current_user = get_current_user()
+
+        # Cek apakah pengguna memiliki foto profil
+        if current_user.photo_profile is None:
+            return jsonify({
+                'error': True,
+                'message': 'User saat ini tidak mempunyai foto profil'
+            }), 404
+
+        # Menghapus foto profil dari Google Cloud Storage
+        try:
+            # Mendapatkan nama file dari URL foto profil
+            filename = current_user.photo_profile.split('/')[-1]
+
+            # Menghapus foto profil dari Google Cloud Storage
+            bucket = storage_client.bucket(app.config['GCS_BUCKET_NAME'])
+            blob = bucket.blob(filename)
+            blob.delete()
+        except Exception as e:
+            return jsonify({
+                'error': True,
+                'message': 'Gagal menghapus foto profil',
+                'details': str(e)
+            }), 500
+
+        # Menghapus foto profil dari basis data
+        try:
+            cursor.execute("UPDATE auth_model SET photo_profile = NULL WHERE email = %s", (current_user.email,))
+            mysql.commit()
+        except Exception as e:
+            return jsonify({
+                'error': True,
+                'message': 'Gagal menghapus foto profil',
+                'details': str(e)
+            }), 500
+
+        return jsonify({
+            'error': False,
+            'message': 'User saat ini tidak mempunyai foto profil'
+        })
 
 api.add_resource(RegisterUser, "/api/register", methods=["POST"])
 api.add_resource(LoginUser, "/api/login", methods=["POST"])
@@ -420,6 +547,8 @@ api.add_resource(ChangePassword, "/api/changepassword", methods=["POST"])
 api.add_resource(Predict, "/api/predict")  
 api.add_resource(Question, "/api/questions", methods=["GET"])
 api.add_resource(Personality, "/api/personality", methods=["GET"])
+api.add_resource(UploadPhoto, "/api/uploadphoto")
+api.add_resource(DeletePhoto, '/api/deletephoto')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
